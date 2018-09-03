8461d3098c62937f5d120d0120af05c8803c5e94
ymah <yhamid1982@gmail.com>
2018-09-02 23:26:59 -0400

    all gifs displaying, all buttons working, adding emotion working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 let emotions = ['happy', 'sad', 'angry', 'excited', 'nervous', 'scared']
+let rating = ''
+let stillGifUrl = ''
+let gifUrl = ''
+let info = {}
 
 $(document).ready(function () {
   const append = (elm, content) => $(elm).append(content)
@@ -9,7 +13,9 @@ $(document).ready(function () {
   }
   const addNewButton = () => emotions.forEach(x => append('.buttons', button(x)))
   const empty = x => $(x).empty()
-  const getText = x => $(x).text()
+  const getText = (x) => $(x).text()
+  const setText = (x, y) => $(x).text(y)
+  const setAttr = (elm, attr, str) => $(elm).attr(attr, str)
   let searchString = ''
 
   $(document).on('click', '#submit', function () {
@@ -22,22 +28,42 @@ $(document).ready(function () {
   addNewButton()
 
   const apiKey = 'bpkQwq73xVYAAVhMnuooa6rnvD0fTDKA'
+  const setVars = function (x) {
+    rating = x.rating
+    title = x.title
+    stillGifUrl = x.images.fixed_width_still.url
+    gifUrl = x.images.original.url
+  }
+
+  const displayGif = function (x, i) {
+    let id = '#' + i
+    setAttr(id + ' img', 'src', stillGifUrl)
+    setText(id + ' .rating', 'Rating: ' + rating)
+  }
+
+  const displayAll = function (x, i) {
+    setVars(x)
+    displayGif(x, i)
+  }
 
   const apiCall = function (x) {
-    let queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + x + '&limit=12'
+    let queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + x + '&limit=12&rating=pg-13'
     $.ajax({
       url: queryUrl,
       method: 'GET'
     }).then(function (response) {
-      let rating = response.rating
-      let title = response.title
-      let stillGifUrl = response.images.orginal_still.url
-      let gifUrl = response.images.original.url
-      console.log(response)
+      info = response.data
+      info.forEach((x, i) => displayAll(x, i))
     })
   }
+
   $(document).on('click', '.emotion', function () {
     searchString = getText(this)
     apiCall(searchString)
+    console.log(searchString)
+    // console.log(rating)
+    // console.log(title)
+    // console.log(stillGifUrl)
+    // console.log(gifUrl)
   })
 })
